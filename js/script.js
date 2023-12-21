859c35fc18837ed55e21bb3c21614f357d934a77
Zariy7 <nazariy.hereha@gmail.com>
2023-12-21 08:59:35 +0100

    fixed for reusability

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ createApp({
     },
     methods:{
         generateEmail(){
+            this.emails = [];
+            
             for(let i = 0; i<10; i++){
                 axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then( (email) =>{
                     //console.log(email.data.response);
